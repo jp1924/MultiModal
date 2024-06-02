a3770003f266b152fd8d6264f3a877aace8195c1
jp1924 <jsb10121249@gmail.com>
2024-06-02 03:05:54 +0000

    Add: collator

--- data/collator.py
+++ data/collator.py
@@ -1,146 +1,54 @@
+from copy import deepcopy
 from dataclasses import dataclass
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Union
 
 import torch
+from models import MplugOwlProcessor
 
-from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2ForPreTraining, Wav2Vec2Processor
 from transformers.data.data_collator import DataCollatorMixin
-from transformers.models.wav2vec2.modeling_wav2vec2 import (
-    _compute_mask_indices,
-    _sample_negative_indices,
-)
-from models import 
 
 
 @dataclass
-class DataCollatorForUReader(DataCollatorMixin):
-    """
-    Data collator that will dynamically pad the inputs received and prepare masked indices
-    for self-supervised pretraining.
-
-    Args:
-        model (:class:`~transformers.Wav2Vec2ForPreTraining`):
-            The Wav2Vec2 model used for pretraining. The data collator needs to have access
-            to config and ``_get_feat_extract_output_lengths`` function for correct padding.
-        feature_extractor (:class:`~transformers.Wav2Vec2FeatureExtractor`):
-            The processor used for proccessing the data.
-        padding (:obj:`bool`, :obj:`str` or :class:`~transformers.tokenization_utils_base.PaddingStrategy`, `optional`, defaults to :obj:`True`):
-            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
-            among:
-            * :obj:`True` or :obj:`'longest'`: Pad to the longest sequence in the batch (or no padding if only a single
-              sequence if provided).
-            * :obj:`'max_length'`: Pad to a maximum length specified with the argument :obj:`max_length` or to the
-              maximum acceptable input length for the model if that argument is not provided.
-            * :obj:`False` or :obj:`'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of
-              different lengths).
-        max_length (:obj:`int`, `optional`):
-            Maximum length of the ``input_values`` of the returned list and optionally padding length (see above).
-        pad_to_multiple_of (:obj:`int`, `optional`):
-            If set will pad the sequence to a multiple of the provided value.
-            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
-            7.5 (Volta).
-        mask_time_prob (:obj:`float`, `optional`, defaults to :obj:`0.65`):
-            Percentage (between 0 and 1) of all feature vectors along the time axis which will be masked for the contrastive task.
-            Note that overlap between masked sequences may decrease the actual percentage of masked vectors.
-            The default value is taken from the original wav2vec 2.0 article (https://arxiv.org/abs/2006.11477),
-            and results in about 49 percent of each sequence being masked on average.
-        mask_time_length (:obj:`int`, `optional`, defaults to :obj:`10`):
-            Length of each vector mask span to mask along the time axis in the contrastive task. The default value
-            originates from the original wav2vec 2.0 article and corresponds to the ``M`` variable mentioned there.
-    """
-
-    feature_extractor: Wav2Vec2FeatureExtractor
+class DataCollatorForMplugOwl(DataCollatorMixin):
+    img_token_ids: int
+    response_token_ids: List[int]
+    processor: MplugOwlProcessor
     padding: Union[bool, str] = "longest"
     return_tensors: str = "pt"
 
     def torch_call(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
-        # features가 2차원 리스트로 들어올 떄 feature_extractor에서 padding을 진행하지 못함. 따라서 이걸 1차원 리스트로 변경 함.
-        if len(features[0]["input_values"].shape) == 2:
-            features = [{"input_values": x["input_values"][0]} for x in features]
+        input_ids = []
+        for x in features:
+            if x["input_ids"][-1] != self.processor.tokenizer.eos_token_id:
+                eos_token = torch.tensor([self.processor.tokenizer.eos_token_id])
+                x["input_ids"] = torch.cat([x["input_ids"], eos_token])
+
+            input_ids.append({"input_ids": x["input_ids"]})
 
         # reformat list to dict and set to pytorch format
-        batch = self.feature_extractor.pad(
-            features,
+        batch = self.processor.tokenizer.pad(
+            input_ids,
             padding=self.padding,
-            pad_to_multiple_of=self.pad_to_multiple_of,
             return_attention_mask=True,
             return_tensors=self.return_tensors,
         )
 
-        device = batch["input_values"].device
-        batch_size = batch["input_values"].shape[0]
+        labels = deepcopy(batch["input_ids"])
 
-        mask_indices_seq_length = self.model._get_feat_extract_output_lengths(batch["input_values"].shape[-1])
-        # make sure masked sequence length is a Python scalar
-        mask_indices_seq_length = int(mask_indices_seq_length)
+        img_token_mask = labels == self.img_token_ids
+        max_length = labels.shape[1]
 
-        # make sure that no loss is computed on padded inputs
-        # evaluate에선 sub_attention_mask 사용을 안함. 그리고 없애는 처리도 하지 않기 때문에 애러가 발생함.
-        if batch.get("attention_mask") is not None and self.model.training:
-            # compute real output lengths according to convolution formula
-            batch["sub_attention_mask"] = self.model._get_feature_vector_attention_mask(
-                mask_indices_seq_length, batch["attention_mask"]
-            )
+        _, response_token_pos = torch.where(labels == self.response_token_ids[0])
+        response_token_pos += len(self.response_token_ids)
 
-        features_shape = (batch_size, mask_indices_seq_length)
+        mask_pos = [torch.cat([torch.ones(x), torch.zeros(max_length - x)]) for x in response_token_pos]
+        mask_pos = torch.stack(mask_pos)
 
-        # sample randomly masked indices
-        mask_time_indices = _compute_mask_indices(
-            features_shape,
-            self.mask_time_prob,
-            self.mask_time_length,
-            attention_mask=batch.get("sub_attention_mask"),
-            min_masks=self.mask_time_min_masks,
-        )
-
-        if not (mask_time_indices.sum(-1) - 1).all():
-            breakpoint()
-        # sample negative indices
-        sampled_negative_indices = _sample_negative_indices(
-            features_shape,
-            self.model.config.num_negatives,
-            mask_time_indices=mask_time_indices,
-        )
-        batch["mask_time_indices"] = torch.tensor(mask_time_indices, dtype=torch.long, device=device)
-        batch["sampled_negative_indices"] = torch.tensor(sampled_negative_indices, dtype=torch.long, device=device)
-
-        return batch
-
-
-@dataclass
-class DataCollatorFormPlugOwl(DataCollatorMixin):
-    processor: Wav2Vec2Processor
-    padding: Union[bool, str] = "longest"
-    return_tensors: str = "pt"
-    pad_to_multiple_of: Optional[int] = None
-    pad_to_multiple_of_labels: Optional[int] = None
-    feature_extractor_input_name: Optional[str] = "input_values"
-
-    def torch_call(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
-        input_features = [
-            {self.feature_extractor_input_name: feature[self.feature_extractor_input_name][0]} for feature in features
-        ]
-        label_features = [{"input_ids": feature["labels"]} for feature in features]
-
-        batch = self.processor.pad(
-            input_features=input_features,
-            padding=self.padding,
-            pad_to_multiple_of=self.pad_to_multiple_of,
-            return_tensors="pt",
-        )
-
-        labels_batch = self.processor.pad(
-            labels=label_features,
-            padding=self.padding,
-            pad_to_multiple_of=self.pad_to_multiple_of_labels,
-            return_tensors="pt",
-        )
-
-        # replace padding with -100 to ignore loss correctly
-        labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
+        labels[mask_pos.bool()] = -100
+        labels[~batch["attention_mask"].bool()] = -100
+        labels[img_token_mask] = self.img_token_ids
 
         batch["labels"] = labels
-        if "attention_mask" in batch:
-            batch["attention_mask"] = batch["attention_mask"].to(torch.long)
+        batch["pixel_values"] = torch.stack([x["pixel_values"] for x in features])
 
         return batch
